5503ed3de5315a1854c2f5c49b2f2f2b73f1137e
Michail Fragkiskos <support@tecdynamics.co.uk>
2024-04-10 19:20:02 +0100

    2.1

--- src/Providers/ShortcodeServiceProvider.php
+++ src/Providers/ShortcodeServiceProvider.php
@@ -23,15 +23,9 @@ class ShortcodeServiceProvider extends ServiceProvider
         });
 
         $this->app->singleton('view', function ($app) {
-            // Next we need to grab the engine resolver instance that will be used by the
-            // environment. The resolver will be used by an environment to get each of
-            // the various engine implementations such as plain PHP or Blade engine.
             $resolver = $app['view.engine.resolver'];
             $finder = $app['view.finder'];
             $env = new Factory($resolver, $finder, $app['events'], $app['shortcode.compiler']);
-            // We will also set the container instance on this view environment since the
-            // view composers may be classes registered in the container, which allows
-            // for great testable, flexible composers for the application developer.
             $env->setContainer($app);
             $env->share('app', $app);
 
